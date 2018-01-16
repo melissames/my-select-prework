1bfe570bc8f7bccb6b57b983dfb2f8ae2498fb1a
Melissa Sames <melissa.sames@gmail.com>
2018-01-16 20:11:30 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ new_collection = []
+
+ while i < collection.length
+   yield collection[i]
+   if collection i == true
+     new_collection << collection[i]
+  end
+  i = i + 1
+end
+new_collection
 end
